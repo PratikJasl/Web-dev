0d7720012036db9cfec9f34ca9eb3dae1b039777
PratikJussal <pratik.jussal@tataelectronics.co.in>
2026-02-19 13:49:56 +0530

    Web Dev

---
+++ Sign-Up-Form/script.js
@@ -0,0 +1,26 @@
+// arr = [1,2,3,4]
+
+// console.log(arr != null);
+// console.log(typeof(arr) == "object")
+
+// if(typeof(arr) == "object" || arr != null){
+//     console.log("array Is an Object");
+// }else{
+//     console.log("filled array");
+// }
+
+function hello(){
+    return "This is JS Revision"
+}
+
+let anon = function(a, b){
+    return a + b
+}
+
+(function add(a,b){
+    p
+})
+
+result = hello();
+result2 = anon(5,6);
+console.log(result,"\n", result2);
